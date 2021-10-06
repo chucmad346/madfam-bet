1988382103c4e03092e13a02bff6dcba4ec5c5dd
Madfam <maduagwufrancis@yahoo.com>
2021-10-06 13:17:10 -0500

    add register file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //javascript//
+$(myLoad);
+var myLoad = function(){$('reg').click(function(e){
+    e.preventDefault();
+    $('form').load(this.href);
+    console.log("check");
+})
+}
---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="form-control">
+    <input type="text" name="username" placeholder="username" required>
+</div>
+<div class="form-control">
+    <input type="email" name="email" placeholder="email" required>
+</div>
+<div class="form-control">
+    <input type="password" name="password" placeholder="password" required>
+</div>
+<button class="btn">Login</button>
+</body>
+</html>
